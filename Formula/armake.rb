4b656fa525c7525e35a7900fec6ce93799ce84ed
Björn Dahlgren <bjorn@dahlgren.at>
2018-10-08 23:38:46 +0200

    v0.6.3

---
+++ Formula/armake.rb
@@ -0,0 +1,21 @@
+class Armake < Formula
+  desc "Arma modding tools (PAA conversion, binarization/rapification, PBO packing)"
+  homepage "https://github.com/KoffeinFlummi/armake"
+  url "https://github.com/KoffeinFlummi/armake/archive/v0.6.3.tar.gz"
+  sha256 "1c3010d4745851b91129367197e9066281ad1dbbe015e9fa5b25a386509ff408"
+  head "https://github.com/KoffeinFlummi/armake.git"
+
+  depends_on "bison" => :build
+  depends_on "openssl" => :build
+
+  def install
+    inreplace "Makefile", "  ", "\t"
+    system "make"
+    bin.install Dir["bin/*"]
+    bash_completion.install "completions/armake" => "armake"
+  end
+
+  test do
+    system "#{bin}/armake"
+  end
+end
